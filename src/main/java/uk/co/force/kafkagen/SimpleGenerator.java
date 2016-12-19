633145084090eb55a97fe888f27411b0efbd5a67
tsellers <tsellers@salesforce.com>
2016-12-19 16:41:31 +0000

    changes

--- src/main/java/uk/co/force/kafkagen/SimpleGenerator.java
+++ src/main/java/uk/co/force/kafkagen/SimpleGenerator.java
@@ -1,7 +1,13 @@
 package uk.co.force.kafkagen;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,9 +18,15 @@ import uk.co.force.kafkagen.services.MessageService;
 
 public class SimpleGenerator {
 
-	private Logger logger;
-	private MessageService messageService;
-	private Producer<byte[], byte[]> kafkaProducer;
+	protected Logger logger;
+	protected MessageService messageService;
+	protected Producer<byte[], byte[]> kafkaProducer;
+	
+	public static void main (String[] args) {
+		
+		new SimpleGenerator();
+
+	}	
 	
 	public SimpleGenerator() {
 		
@@ -25,11 +37,12 @@ public class SimpleGenerator {
 		// in the ApplicationInjector class
 		Injector injector = Guice.createInjector(new ApplicationInjector());
 		MessageService messageService = injector.getInstance(MessageService.class);
-		
-		logger.info("{}", messageService.getMessageData());
-		
+
 		KafkaConfig config = new KafkaConfig();
 		Producer<byte[], byte[]> kafkaProducer = new KafkaProducer<>(config.getKafkaProps());
+		
+		Timer timer = new Timer();
+		timer.schedule(new ProducerTask(), 0, 1000); //start immediately and post every second
 		
 		Runtime.getRuntime().addShutdownHook(new Thread() {
 			@Override
@@ -38,14 +51,29 @@ public class SimpleGenerator {
 				kafkaProducer.close();
 			}
 		});
-	}
+	}	
 	
-	public static void main (String[] args) {
-		
-		new SimpleGenerator();
+	class ProducerTask extends TimerTask {
 
+		@Override
+		public void run() {
+			String message = messageService.getMessageData();
+			logger.info("Getting ready to send message :- \n{}", message);
+			ProducerRecord<byte[], byte[]> producerRecord = new ProducerRecord<byte[], byte[]>("interactions", message.getBytes());
+			
+			// send and process asynchronous
+			kafkaProducer.send(producerRecord, new Callback() {
+				@Override
+				public void onCompletion(RecordMetadata metadata, Exception exception) {
+					if(exception != null) 
+						exception.printStackTrace();
+					else
+						logger.info("We sent message to Kafka Topic as offset {}", metadata.offset());
+				}
+			});
+		}
+		
 	}
-	
 	
 	
 	
